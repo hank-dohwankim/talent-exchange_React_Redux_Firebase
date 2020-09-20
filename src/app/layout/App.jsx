cce595ee980d81b87cea93d98eebf018f5201e0a
hank.dohwankim <hank.dohwankim@gmail.com>
2020-09-20 17:56:00 -0500

    navbar route

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage';
+import EventForm from '../../features/events/eventForm/EventForm';
+import HomePage from '../../features/home/HomePage';
 import NavBar from '../../features/nav/NavBar';
 
 function App() {
@@ -20,12 +24,10 @@ function App() {
     <>
       <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className="main">
-        <EventDashboard
-          formOpen={formOpen}
-          setFormOpen={setFormOpen}
-          selectEvent={handleSelectedEvent}
-          selectedEvent={selectedEvent}
-        />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/events" component={EventDashboard} />
+        <Route path="/events/:id" component={EventDetailedPage} />
+        <Route path="/createEvent" component={EventForm} />
       </Container>
     </>
   );
---
+++ src/features/home/HomePage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  Segment,
+  Image,
+  Button,
+  Icon,
+} from 'semantic-ui-react';
+
+export default function HomePage() {
+  return (
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            style={{ marginBottom: 12 }}
+          ></Image>
+          Talent Exchange
+        </Header>
+        <Button size="huge" inverted>
+          Get started
+          <Icon name="right arrow" inverted />
+        </Button>
+      </Container>
+    </Segment>
+  );
+}
