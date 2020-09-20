f613b0c638a1cc0e9cc5c60d7902a3a7ae8638b5
hank.dohwankim <hank.dohwankim@gmail.com>
2020-09-20 13:58:15 -0500

    add edit, delete event function

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -4,16 +4,49 @@ import EventList from './EventList';
 import EventForm from '../eventForm/EventForm';
 import { sampleData } from '../../../app/api/samepleData';
 
-export default function EventDashboard({ formOpen, setFormOpen }) {
+export default function EventDashboard({
+  formOpen,
+  setFormOpen,
+  selectEvent,
+  selectedEvent,
+}) {
   const [events, setEvents] = useState(sampleData);
+
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+  }
+
+  function handleUpdateEvent(updateEvent) {
+    setEvents(
+      events.map((event) => (event.id === updateEvent.id ? updateEvent : event))
+    );
+    selectEvent(null);
+  }
+
+  function handleDeleteEvent(eventId) {
+    setEvents(events.filter((event) => event.id !== eventId));
+  }
 
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={events} />
+        <EventList
+          events={events}
+          selectEvent={selectEvent}
+          deleteEvent={handleDeleteEvent}
+        />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && <EventForm setFormOpen={setFormOpen} />}
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            selectedEvent={selectedEvent}
+            createEvent={handleCreateEvent}
+            updateEvent={handleUpdateEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+          />
+        )}
       </Grid.Column>
     </Grid>
   );
