652160d97eac081c1df0236b1736b0523f9f3c6d
hank.dohwankim <hank.dohwankim@gmail.com>
2020-09-22 23:11:38 -0500

    add scroll top, reducer in event detail

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -6,24 +6,10 @@ import { useSelector } from 'react-redux';
 export default function EventDashboard() {
   const { events } = useSelector((state) => state.event);
 
-  // function handleCreateEvent(event) {
-  //   setEvents([...events, event]);
-  // }
-
-  // function handleUpdateEvent(updateEvent) {
-  //   setEvents(
-  //     events.map((event) => (event.id === updateEvent.id ? updateEvent : event))
-  //   );
-  // }
-
-  function handleDeleteEvent(eventId) {
-    // setEvents(events.filter((event) => event.id !== eventId));
-  }
-
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={events} deleteEvent={handleDeleteEvent} />
+        <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
         <h2>Event Filters</h2>
