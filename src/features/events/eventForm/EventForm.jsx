cbae20cfedf5d47917872ba5cb2b86cd3b34676b
hank.dohwankim <hank.dohwankim@gmail.com>
2020-09-23 22:10:12 -0500

    Text area, text input and select input option added

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -7,6 +7,9 @@ import { createEvent, updateEvent } from '../eventActions';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../app/common/form/MyTextInput';
+import MyTextArea from './../../../app/common/form/MyTextArea';
+import MySelectInput from './../../../app/common/form/MySelectInput';
+import { categoryData } from './../../../app/api/categoryOptions';
 
 export default function EventFrom({ match, history }) {
   const dispatch = useDispatch();
@@ -55,8 +58,12 @@ export default function EventFrom({ match, history }) {
         <Form className="ui form">
           <Header sub color="teal" content="Event Details" />
           <MyTextInput name="title" placeholder="Event title" />
-          <MyTextInput name="category" placeholder="Event category" />
-          <MyTextInput name="description" placeholder="Description" />
+          <MySelectInput
+            name="category"
+            placeholder="Event category"
+            options={categoryData}
+          />
+          <MyTextArea name="description" placeholder="Description" rows={3} />
 
           <Header sub color="teal" content="Event Location Details" />
           <MyTextInput name="city" placeholder="City" />
